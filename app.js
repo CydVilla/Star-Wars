c6f80d8a869b652bc509de22e0676d78686288a5
Cyd Villavicencio <Villacv@Cyds-MBP.fios-router.home>
2021-04-21 19:46:30 -0400

    completed battle simulation with while loop/conditional, started styling process with CSS/Flexbox

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const duel = document.querySelector("#duel");
 const character1 = document.querySelector("#character1");
 const character2 = document.querySelector("#character2");
 const battleArena = ["Death-Star.jpeg", "mustafar.png", "tatooine.jpeg"];
+const battle = (character1, character2) => {
+  console.log("battle", character1.hp);
+  while (character1.hp > 0 && character2.hp > 0) {
+    character1.hp -= character2.attack();
+    character2.hp -= character1.attack();
+  }
+  console.log(typeof character1.hp, typeof character2.hp);
+  if (character1.hp === 0) {
+    console.log(character2.name + "is the victor!");
+  } else {
+    console.log(character1.name + "is the victor!");
+  }
+};
+const winner = (character1, character2) => {
+  battle(character1, character2);
+};
 // functionExpression syntax: variable = functionName, functionName = anonymous function
 const randomCharNumber = () => {
   return Math.ceil(Math.random() * 83);
@@ -60,7 +76,7 @@ const attributes = (name) => {
     attack: () => {
       let randomAttack = Math.ceil(Math.random() * 3);
       if (randomAttack > 0) {
-        console.log("heavy attack", randomAttack());
+        return randomAttack;
       }
     },
   };
@@ -78,27 +94,32 @@ duel.addEventListener("click", () => {
   body.style.backgroundImage = `url(images/${battleArenaImage})`;
   // let char1 = getCharacter()
   // char1.then(data => console.log(data))
-  Promise.all([getCharacter(), getCharacter2()]).then((values) => {
-    values.forEach((character, index) => {
-      console.log(character.hp);
-      if (index == "0") {
-        let name = document.createElement("div");
-        let hitPoints = document.createElement("span");
-        hitPoints.innerHTML = character.hp;
-        character1.appendChild(hitPoints);
-        name.innerHTML = character.name;
-        character1.appendChild(name);
-      } else {
-        let name = document.createElement("div");
-        let hitPoints = document.createElement("span");
-        hitPoints.innerHTML = character.hp;
-        character2.appendChild(hitPoints);
-        name.innerHTML = character.name;
-        character2.appendChild(name);
-      }
+  Promise.all([getCharacter(), getCharacter2()])
+    .then((values) => {
+      values.forEach((character, index) => {
+        console.log(character.hp);
+        if (index == "0") {
+          let name = document.createElement("div");
+          let hitPoints = document.createElement("span");
+          hitPoints.innerHTML = character.hp;
+          character1.appendChild(hitPoints);
+          name.innerHTML = character.name;
+          character1.appendChild(name);
+        } else {
+          let name = document.createElement("div");
+          let hitPoints = document.createElement("span");
+          hitPoints.innerHTML = character.hp;
+          character2.appendChild(hitPoints);
+          name.innerHTML = character.name;
+          character2.appendChild(name);
+        }
+      });
+      return values;
+    })
+    .then((characters) => {
+      console.log(characters);
+      winner(characters[0], characters[1]);
     });
-    console.log(values);
-  });
 });
 
 // append design, create elements, create background images, after character creation add HP number..., One button for a random attack, end game script, flexbox,
